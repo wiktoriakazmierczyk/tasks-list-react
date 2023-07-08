2e5af37330818f3f10c2374fb23d7d15b71be7b1
Wiktoria Kazmierczyk <wiktoriakazmierczyk@protonmail.com>
2023-07-08 23:05:33 +0200

    toggle task done interaction

--- src/App.js
+++ src/App.js
@@ -22,6 +22,17 @@ function App() {
 		setTasks(tasks => tasks.filter(task => task.id !== id));
 	};
 
+	const toggleTaskDone = (id) => {
+		setTasks(tasks => tasks.map(task => {
+			if (task.id === id) {
+				return { ...task, done: !task.done };
+			}
+
+			return task;
+		}));
+	};
+
+
 	return (
 		<Container>
 			<Header title="Lista zadań" />
@@ -33,7 +44,13 @@ function App() {
 
 			<Section
 				title="Lista zadań"
-				body={<Tasks tasks={tasks} hideDone={hideDone} removeTask={removeTask} />}
+				body={
+					<Tasks
+						tasks={tasks}
+						hideDone={hideDone}
+						removeTask={removeTask}
+						toggleTaskDone={toggleTaskDone}
+					/>}
 				extraHeaderContent={
 					<Buttons
 						tasks={tasks}
