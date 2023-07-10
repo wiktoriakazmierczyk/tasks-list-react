7d29ab0507a6f99ae30b72f57a488e8f3e5aa661
Wiktoria Kazmierczyk <wiktoriakazmierczyk@protonmail.com>
2023-07-10 20:16:28 +0200

    marking task as done

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -10,7 +10,7 @@ const Tasks = ({ tasks, hideDone, removeTask, toggleTaskDone }) => (
                 >
                     {task.done ? "✔" : ""}
                 </button>
-                <span className={`tasks__content${task.done ? "tasks__content--done" : ""}`}>
+                <span className={`tasks__content${task.done ? " tasks__content--done " : ""}`}>
                     {task.content}
                 </span>
                 <button
